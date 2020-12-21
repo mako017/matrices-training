d6a595a29887b837b5a14577151938c501ef0401
Marco Koch <marco.koch@uni-saarland.de>
2020-12-21 13:15:24 +0100

    removed unused components

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import home from "@/views/home.vue";
 import builder from "@/views/builder.vue";
-import database from "@/views/database.vue";
-import editor from "@/views/editor.vue";
-
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: home,
-  },
-  {
-    path: "/builder",
-    name: "Builder",
-    component: builder,
-  },
-  {
-    path: "/database",
-    name: "Database",
-    component: database,
-  },
-  {
-    path: "/editor",
-    name: "Editor",
-    component: editor,
-  },
+	{
+		path: "/",
+		name: "Home",
+		component: builder,
+	},
 ];
 
 const router = new VueRouter({
-  routes,
+	routes,
 });
 
 export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import builder from "@/store/modules/builder";
-import database from "@/store/modules/database";
-import editor from "@/store/modules/editor";
-import userAuth from "@/store/modules/userAuth";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
 	modules: {
 		builder,
-		database,
-		editor,
-		userAuth,
 	},
 });
